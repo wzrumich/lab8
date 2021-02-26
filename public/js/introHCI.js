e019b3322188c679326054720d57840092ddfdc6
Zerui Wei <wezerui@umich.edu>
2021-02-25 20:47:52 -0500

    furst

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,8 +10,8 @@ $(document).ready(function() {
  */
 function initializePage() {
 	console.log("Page ready");
- 	// initCamera();
- 	// initMap();
+ 	initCamera();
+ 	initMap();
  	initGestures();
  	initRSVPForm();
 }
@@ -19,9 +19,30 @@ function initializePage() {
 // init jQuery gestures  
 function initGestures() {
 	// add gestures listener here
+	$(function(){
+		$(".judge-img").bind("taphold", tapholdHandler);
+
+		function tapholdHandler(event){
+			//get the id of the event source
+			var targetIDPrefix = event.target.id;
+			console.log("got prefix: " + targetIDPrefix );
+			//show bio
+			$("#" + targetIDPrefix + "-bio").show();
+		}
+	});
 }
 
 // init RSVP form submit listener
 function initRSVPForm() {
   // add your code here
+  $('#rsvpForm').submit(function(e){
+		e.preventDefault();
+		console.log("submitting form...")
+		var rsvpEmail = $('#rsvpEmail').val();
+		$.post('addRSVP', { rsvpEmail: rsvpEmail}, postCallback);
+	});
+	function postCallback(response){
+		alert("RSVP form successfully submitted!");
+		$('#rsvpEmail').val('');
+	}
 }
--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,11 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'tcjMeDGb9ALxZxMDsGVFq9MnwpXZwJOj';
+		var map = L.mapquest.map('map', {
+		center: [32.88, -117.236],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false
+	});
+	L.marker([32.88, -117.236]).addTo(map);
 }
